ccf0b1c02b5f9d563d8105074d953246a002c0f4
Gijsbert van Dalen <43387429+Gijsbertdalen@users.noreply.github.com>
2018-10-27 17:31:45 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,11 +20,11 @@ html = scraperwiki.scrape('https://web.archive.org/web/20120318184750/http://www
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td> tags
-print tds
+# print tds
 
-# for td in tds:
-#    print lxml.html.tostring(td) # the full HTML tag
-#    print td.text                # just the text inside the HTML tag
+for td in tds:
+    print lxml.html.tostring(td) # the full HTML tag
+    print td.text                # just the text inside the HTML tag
 
 # -----------------------------------------------------------------------------
 # 2. Save the data in the ScraperWiki datastore.
