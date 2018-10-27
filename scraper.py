e522834e49dd8c34cd25550c228ae7b37c2f12b8
Gijsbert van Dalen <43387429+Gijsbertdalen@users.noreply.github.com>
2018-10-27 17:22:43 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@
 
 import scraperwiki
 html = scraperwiki.scrape('https://web.archive.org/web/20120318184750/http://www.inmo.ie/6022')
-print "Click on the ...more link to see the whole page"
-print html
+# print "Click on the ...more link to see the whole page"
+# print html
 
 # -----------------------------------------------------------------------------
 # 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
@@ -19,9 +19,9 @@ print html
 
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td> tags
 print tds
 
-# tds = root.cssselect('td') # get all the <td> tags
 # for td in tds:
 #    print lxml.html.tostring(td) # the full HTML tag
 #    print td.text                # just the text inside the HTML tag
