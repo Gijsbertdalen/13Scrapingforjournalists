6cdf65e88670c9ddbc03df9d85301e3d920d4d12
Gijsbert van Dalen <43387429+Gijsbertdalen@users.noreply.github.com>
2018-10-28 13:40:10 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -34,7 +34,7 @@ tds = root.cssselect('td') # get all the <td> tags
 # -----------------------------------------------------------------------------
 
 for td in tds:
-    record = { "td" : td.tag } # column name and value
+    record = { "td" : td.attrib['href'] } # column name and value
     try:
         scraperwiki.sqlite.save(["td"], record) # save the records one by one
     except:
